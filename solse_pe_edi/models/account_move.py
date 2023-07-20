ca97f3f1224a4ca61af8962585d788b0a92156bb
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2023-07-20 17:27:31 -0500

    UPDATE - Por Facturación
    Actualizado por falla en módulo de facturación.

--- solse_pe_edi/models/account_move.py
+++ solse_pe_edi/models/account_move.py
@@ -125,9 +125,13 @@ class AccountMove(models.Model):
 		
 	@api.depends('journal_id', 'partner_id', 'company_id', 'move_type')
 	def _compute_l10n_latam_available_document_types(self):
-		self.l10n_latam_available_document_type_ids = False
+		for reg in self:
+			reg.l10n_latam_available_document_type_ids = False
+
 		for rec in self.filtered(lambda x: x.journal_id and x.l10n_latam_use_documents and x.partner_id):
 			dominio = rec._get_l10n_latam_documents_domain()
+			_logging.info("dominio a buscar ")
+			_logging.info(dominio)
 			rec.l10n_latam_available_document_type_ids = self.env['l10n_latam.document.type'].search(dominio)
 		
 
