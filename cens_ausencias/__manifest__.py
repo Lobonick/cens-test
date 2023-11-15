bc6a137e08d3bdb1a94bee8bfbcb6e4e499e80fe
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2023-11-15 00:49:27 -0500

    UPGRADE - Ausencias 3-20

--- cens_ausencias/__manifest__.py
+++ cens_ausencias/__manifest__.py
@@ -12,11 +12,11 @@
 	'author': "Área de Sistemas - CENS-PERÚ",
     "website": "https://www.cens.com.pe",
 	'category': 'Human Resources/Time-Off',
- 	'version': '1.0.3.01',
+ 	'version': '1.0.3.20',
+    'license': 'Other proprietary',
 	'depends': ['base','hr','hr_holidays','calendar','resource'],
 	'data': ['views/hr_leave_view_form_cens.xml'],
  	'installable': True,
 	'application': True,
-	'auto_install': False,
  	'icon': "https://sisac-peru.com/CENS-LOGO%20-%20Baja%20-%20Transparente.png",
 }
--- cens_ausencias/models/models.py
+++ cens_ausencias/models/models.py
@@ -1,6 +1,16 @@
-from odoo import api, fields, models
-from datetime import datetime
+# -*- coding: utf-8 -*-
+# Complemento de adaptación ODOO-CENS (r)-2023
+
+from odoo import models, fields, api
+import logging
+import datetime
+# import random
+# import string
+# from datetime import datetime
 # from datetime import timedelta
+
+_logging = logging.getLogger(__name__)
+
 
 class HrLeaveCustom(models.Model):
     _inherit = 'hr.leave'
@@ -8,21 +18,7 @@ class HrLeaveCustom(models.Model):
     # ---------------------------
     # AGREGA CAMPOS AL MODELO
     # ---------------------------
-    x_cens_codiden = fields.Char(string='CÓDIGO:', readonly=True)
-    x_cens_hora = fields.Datetime(string='Hora:', readonly=True)
-
-    @api.onchange('date_from')
-    def _onchange_date_from(self):
-        for record in self:
-            record.x_hora = record.date_from
-
-    @api.onchange('date_from')
-    def _onchange_date_from(self):
-        # Calcula el código correlativo
-        w_correlativo = ""
-        for record in self:
-            w_correlativo = ("000000"+str(record.id))[-6:]  
-            record.x_cens_codiden = "AU-" + str(record.date_from.year) + "-" + w_correlativo
+    x_cens_codiden = fields.Char(string='CÓDIGO:', readonly=True, existing_field=True)
 
     # ------------------------------
     # CALCULA EL CORRELATIVO
@@ -32,12 +28,14 @@ class HrLeaveCustom(models.Model):
         for record in self:
             w_correlativo = ("000000"+str(record.id))[-6:]  
             record.x_cens_codiden = "AU-" + str(record.date_from.year) + "-" + w_correlativo
-        return True
+        pass
 
-
+#    @api.depends('name') 
 #    @api.onchange('date_from')
 #    def _onchange_date_from(self):
 #        # Obtiene la hora
 #        for record in self:
 #            record.x_hora = datetime.strptime(record.date_from, '%Y-%m-%d %H:%M:%S').strftime('%H:%M')
+#        random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+#        record.x_cens_codiden = "AU-" + str(record.date_from.year) + "-" + w_correlativo + "-" + random_chars
 
