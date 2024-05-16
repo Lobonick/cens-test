843b85b5645ad1878c709725fe77a14d7e584a8d
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2024-05-16 14:47:11 -0500

    UPDATE - crm cens - 1-03

---
+++ cens_crm/__manifest__.py
@@ -0,0 +1,47 @@
+{
+	'name': "CRM - CENS",
+
+	'summary': """
+		Actualizaciones adicionales para el módulo CRM""",
+
+    'description': """
+		Traslada lo hecho en Odoo Studio a código CENS.
+        CENS-PERÚ 
+	""",
+
+	'author': "Área de Sistemas - CENS-PERÚ",
+    "website": "https://www.cens.com.pe",
+	'category': 'Sales/CRM',
+ 	'version': '16.0.1.03',
+    'license': 'Other proprietary',
+    'contributors': [
+        'Enrique Alcántara <ealcantara@cens.com.pe>',
+    ], 
+    'depends': [
+        'crm',
+        'base',
+        'base_setup',
+    ],
+
+	'data': [
+        'views/crm_form_cens.xml',
+        ],
+
+    'assets': {
+            'web.assets_backend': [
+                'cens_crm/static/src/js/popup_message.js',
+            ],
+        },
+
+    'images': [
+        'static/description/comentario.png',
+        'static/description/cens-qrcode.jpg',
+        'static/description/logo-modulos.png'
+    ],
+ 	'installable': True,
+	'application': True,
+	'auto_install': False,
+    'uninstall_hook': '_uninstall_cleanup',
+ 	'icon': "https://sisac-peru.com/CENS-LOGO%20-%20Baja%20-%20Transparente.png",
+    "sequence": 1,
+}
