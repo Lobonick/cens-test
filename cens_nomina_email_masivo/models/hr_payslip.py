868224da36de41d5ac707cfb8e9b4f470624dd2e
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2025-02-20 10:04:05 -0500

    UPDATE - cens nómina email masivo - 1.31

--- cens_nomina_email_masivo/models/hr_payslip.py
+++ cens_nomina_email_masivo/models/hr_payslip.py
@@ -65,7 +65,8 @@ class HrPayslip(models.Model):
                         continue
 
                     _logger.info('Generando PDF de la boleta')
-                    pdf_content, _ = report._render_qweb_pdf([payslip.id])
+                    # pdf_content, _ = report._render_qweb_pdf([payslip.id])
+                    pdf_content, _ = report._render_qweb_pdf([payslip.name])
                     
                     if not pdf_content:
                         _logger.error(f'No se pudo generar el PDF para la boleta {payslip.number}')
