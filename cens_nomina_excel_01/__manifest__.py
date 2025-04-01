423d58851d679e82b501c9c759fa625d2d3aea7c
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2025-04-01 16:59:38 -0500

    UPDATE - cens nómina excel 01 - 3.46

--- cens_nomina_excel_01/__manifest__.py
+++ cens_nomina_excel_01/__manifest__.py
@@ -12,7 +12,7 @@
 	'author': "Área de Sistemas - ODOO-CENS-PERÚ",
     'website': "https://www.cens.com.pe",
     'category': 'Human Resources',
- 	'version': '16.0.3.45',
+ 	'version': '16.0.3.46',
     'license': 'Other proprietary',
     'contributors': [
         'Enrique Alcántara <ealcantara@cens.com.pe>',
