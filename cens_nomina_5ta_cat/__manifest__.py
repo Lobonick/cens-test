7c1743d996956fa409131154d2e2dcbaa8ae2c8a
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2025-05-24 05:51:52 -0500

    UPDATE - cens nómina 5ta cat - 4.13

--- cens_nomina_5ta_cat/__manifest__.py
+++ cens_nomina_5ta_cat/__manifest__.py
@@ -13,7 +13,7 @@
 	'author': "Área de Sistemas - ODOO-CENS-PERÚ",
     'website': "https://www.cens.com.pe",
     'category': 'Human Resources/Payroll',
- 	'version': '16.0.4.12',
+ 	'version': '16.0.4.13',
     'license': 'Other proprietary',
     'contributors': [
         'Enrique Alcántara <ealcantara@cens.com.pe>',
