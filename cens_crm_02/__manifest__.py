e0d6c7a430c4a61bf28fef157688ccbb51057ff4
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2025-08-01 20:36:29 -0500

    UPDATE - cens crm 02 - 1.70

--- cens_crm_02/__manifest__.py
+++ cens_crm_02/__manifest__.py
@@ -20,7 +20,7 @@
 	'author': "Área de Sistemas - CENS-PERÚ",
     "website": "https://www.cens.com.pe",
 	'category': 'Sales/CRM',
- 	'version': '16.0.1.68',
+ 	'version': '16.0.1.70',
     'license': 'Other proprietary',
     'contributors': [
         'Enrique Alcántara <ealcantara@cens.com.pe>',
