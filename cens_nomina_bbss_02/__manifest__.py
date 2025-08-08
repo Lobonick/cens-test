843bc8fa838a1070ae7130e6e7cfc851fb5eeda7
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2025-08-07 23:46:26 -0500

    Add files via upload
    UPDATE - cens nómina bbss 02 - 1.03

--- cens_nomina_bbss_02/__manifest__.py
+++ cens_nomina_bbss_02/__manifest__.py
@@ -13,7 +13,7 @@
 	'author': "Área de Sistemas - ODOO-CENS-PERÚ",
     'website': "https://www.cens.com.pe",
     'category': 'Human Resources/Payroll',
- 	'version': '16.0.1.02',
+ 	'version': '16.0.1.03',
     'license': 'Other proprietary',
     'contributors': [
         'Enrique Alcántara <ealcantara@cens.com.pe>',
