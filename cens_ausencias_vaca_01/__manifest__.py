b2916051151ce0dfa98094f650ac9141baae8bf7
Enrique Alcántara Valderas <114424809+Lobonick@users.noreply.github.com>
2025-12-05 05:42:22 -0500

    UPDATE - cens ausencias vaca01 - 1.03

--- cens_ausencias_vaca_01/__manifest__.py
+++ cens_ausencias_vaca_01/__manifest__.py
@@ -13,7 +13,7 @@
 	'author': "Área de Sistemas - CENS-PERÚ",
     "website": "https://www.cens.com.pe",
 	'category': 'Human Resources/Time-Off',
- 	'version': '16.0.1.02',
+ 	'version': '16.0.1.03',
     'license': 'Other proprietary',
 	'depends': ['base','hr','hr_holidays','calendar','resource'],
     'data': [
